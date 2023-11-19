1ac5761c806929b121aef0c282b1c664b735a8d7
Aditya Khode <113977001+adityakhode@users.noreply.github.com>
2023-11-20 04:44:46 +0530

    Update loginorsignup.py

--- Attendence Monitering System/login/loginorsignup.py
+++ Attendence Monitering System/login/loginorsignup.py
@@ -1,26 +1,27 @@
+import smtplib
+import subprocess
 import customtkinter as ctk
 import tkinter.messagebox as tkmb
+from email.mime.text import MIMEText
 from databases_files import databases as db
-import subprocess
-
+from email.mime.multipart import MIMEMultipart
 
 # Define the name, email, and phone entry fields as global variables
-user_entry = None
-user_pass = None
-name_entry = None
-email_entry = None
-phone_entry = None
-complete_login_button = None
+user_entry             = None
+user_pass              = None
+name_entry             = None
+email_entry            = None
+phone_entry            = None
+complete_login_button  = None
 complete_signup_button = None
-login_button = None
-signup_button = None
-or_label = None
-back_button = None
-checkbox_value = None
-checkbox = None
+login_button           = None
+signup_button          = None
+or_label               = None
+back_button            = None
+checkbox_value         = None
+checkbox               = None
 
 
-#__________________________________________________________________________________________
 def frontend_initiliesd():
     # Selecting GUI theme - dark, light, system (for system default)
     ctk.set_appearance_mode("dark")
@@ -98,10 +99,8 @@ def login_or_signup(action):
         checkbox = ctk.CTkCheckBox(master=app, text="Signup as GFM", variable=checkbox_value, onvalue="on", offvalue="off")
         checkbox.place(relx=0.5, rely=0.7, anchor="center")
 
-        
-
-
 def complete_signup():
+    
     global name_entry, email_entry, phone_entry, user_pass  # Define them as global
     password = user_pass.get()
     name = name_entry.get()
@@ -111,12 +110,14 @@ def complete_signup():
         role = "no"
     else :
         role = "yes"
-    subprocess.run(['python', 'databases.py'])
-    db.insert_user_table(name, phone, email, password,role)
-    db.display()
-    # Display a success message
-    tkmb.showinfo(title="Signup Successful", message=f"User {name} with email {email} and phone number {phone} has been registered successfully!")
-
+        
+    if name and is_valid_phone_number(phone) and has_valid_email_domain(email):
+        subprocess.run(['python', 'databases.py'])
+        db.insert_user_table(name, phone, email, password,role)
+        db.display()
+        # Display a success message
+        tkmb.showinfo(title="Signup Successful", message=f"User {name} with email {email} and phone number {phone} has been registered successfully!")
+        send_email(email)       
 def login():
     global user_entry, user_pass
     entered_username = user_entry.get()  # Get the user-entered username
@@ -131,7 +132,6 @@ def login():
             file.write(db.get_email_id_by_value(entered_username))
             file.close()
         db.conn.commit()
-        app.destroy()
         from login import after_login
         
         
@@ -156,8 +156,61 @@ def back():
     if checkbox:
         checkbox.place_forget()     
     loginpage(app)
+ 
+def is_valid_phone_number(phone_number):
+    try:
+        phone_number = int(phone_number)
 
-    
+        if len(str(phone_number)) == 10:
+            return True
+        else:
+            tkmb.showinfo(title="Error", message="Enter the correct details")
+    except ValueError:
+        tkmb.showinfo(title="Error", message="Enter the correct details")
+   
+def has_valid_email_domain(email):
+    if email.endswith("@moderncoe.edu.in"):
+        return True
+    else:
+        tkmb.showinfo(title="Error", message="Enter the correct details")
+
+def send_email(receiver_email,name):
+    # Sender and receiver email addresses
+    sender_email = "aditya_khode_entc@moderncoe.edu.in"  # Replace with your email address
+    password = "Aditya1996"  # Replace with your email password
+
+    # Create the email message
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = "Welcome to Attendance Monitoring System"
+
+    # Add the welcome message
+    welcome_message = "Hello" + name + """
+    Welcome to the Attendance Monitoring System app! We are delighted to have you on board.
+
+    Thank you for joining our community.
+
+    Best regards,
+    Aditya
+    """
+
+    message.attach(MIMEText(welcome_message, "plain"))
+
+    # Establish a connection to the SMTP server
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        # Start TLS for security
+        server.starttls()
+
+        # Login to the email account
+        server.login(sender_email, password)
+
+        # Send the email
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    print(f"Welcome email sent to {receiver_email} successfully.")
+
+       
 app = frontend_initiliesd()
 loginpage(app)
 app.mainloop()
